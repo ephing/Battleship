b51ea3c4291021b35b8edd657da4e98944d662ce
QiTao Weng <q350w358@ku.edu>
2020-09-10 01:37:55 -0500

    Ensure currentPlayer is global

--- scripts/main.js
+++ scripts/main.js
@@ -10,7 +10,7 @@ main = function () {
     let selector = document.querySelector("#boatSelect");
     let boatCount = selector.options[selector.selectedIndex].value;
     selector.style.visibility = "hidden";
-    let currentPlayer = 2;
+    currentPlayer = 2;
     //stage -1: default value, no controls
     //stage 0: placing boats phase
     //stage 1: choosing where to shoot opponent phase
