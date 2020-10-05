4621eea1056d8710a6039fe7f769bad035fc5126
Thomas Atkins <dev.csnova@gmail.com>
2020-10-04 20:06:44 -0500

    Formated the scorebard

--- scripts/modeSelect.js
+++ scripts/modeSelect.js
@@ -43,6 +43,7 @@ document.addEventListener("DOMContentLoaded", () => {
         showHTML("#inputs");
         showHTML("#boatCount");
         showHTML("#button")
+        showHTML("#scoreboard")
     }
 });
 
